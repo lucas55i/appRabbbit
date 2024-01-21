6b2322ae42fd4c4d8584c3f6726dec11ee895374
lucas55i <lucas_55i@outlook.com>
2024-01-21 19:01:09 -0300

    Refatoração para melhor manutenção.

--- consumer.py
+++ consumer.py
@@ -1,31 +1,36 @@
-import pika 
+from rabbit_connection import RabbitmqConnection
+import logging
+
+rabbitmq_connection = RabbitmqConnection()
+
+
+class RabbitmqConsumer:
+    def __init__(self, callback) -> None:
+        self.queue = "app_queue"
+        self.callback = callback
+        self.channel = rabbitmq_connection.channel
+        self.connect_queue()
+
+    def connect_queue(self): # TODO possivel refatoração
+        self.channel.queue_declare(
+            queue=self.queue,
+            durable=True,
+        )
+
+        self.channel.basic_consume(
+            queue=self.queue, auto_ack=True, on_message_callback=self.callback
+        )
+
+        return self.channel
+
+    def start(self):
+        print(f"listen RabbitMQ on Port 5672")
+        self.channel.start_consuming()
+
 
 def minha_callback(ch, method, properties, body):
     print(body)
 
-# Conexão
-connection_paramters = pika.ConnectionParameters(
-    host="localhost",
-    port=5672,
-    credentials=pika.PlainCredentials(
-        username="guest",
-        password="guest"
-    )
-)
 
-# Conexão a fila
-channel = pika.BlockingConnection(connection_paramters).channel()
-channel.queue_declare(
-    queue='app_queue',
-    durable=True,
-)
-
-# Cria o consumer
-channel.basic_consume(
-    queue='app_queue',
-    auto_ack=True,
-    on_message_callback=minha_callback
-)
-
-print(f'listen RabbitMQ on Port 5672')
-channel.start_consuming()
+rabbitmq_consumer = RabbitmqConsumer(minha_callback)
+rabbitmq_consumer.start()
